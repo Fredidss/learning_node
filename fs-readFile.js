f4972e4d2bd7855da731881031a49c216fdf02a3
Fredidss <fredysgtr@gmail.com>
2024-02-18 11:10:19 -0500

    second commit

--- fs-readFile.js
+++ fs-readFile.js
@@ -1,5 +1,11 @@
 const fs = require('node:fs')
 
+console.log ('Leyendo el primer archivo...')
 const text =  fs.readFileSync('./hmundo.txt', 'utf-8')
 
-console.log (text)
+console.log(text)
+
+console.log ('Leyendo el segundo archivo...')
+const secondText =  fs.readFileSync('./hmundo2.txt', 'utf-8')
+
+console.log(secondText)
